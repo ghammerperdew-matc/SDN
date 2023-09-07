7d75f7f436e189b26157fd9b82005fb6fe5baff9
ghammerperdew-matc <ghammerperdew@madisoncollege.edu>
2023-09-07 15:41:50 -0500

    Update dictionaryPractice2.py

--- Week of Sept 4/dictionaryPractice2.py
+++ Week of Sept 4/dictionaryPractice2.py
@@ -8,64 +8,94 @@ router1 = {
     "hostname": "r1"
     }
 
+
 def print_router(dictionary):
     for key in dictionary.keys():
-        print(f"{key:10}", end="")
+        print(f"{key:13}", end="")
 
-    print("\n" + "-" * 70)
+    print("\n" + "-" * 86)
     
     for value in dictionary.values():
         output = value.split()
-        print(f"{output[0]:10}", end="")
+        print(f"{output[0]:13}", end="")
 
     return(None)
 
-print_router(router1)
 
-while(True):
+def check_address(IP_address):
+        #split IP address into individual octets -- returns a list
+        address = IP_address.split(".")
 
-    print("Would you like to change the mgmtIP address?")
-    userInput = str(input("Enter Y or N: "))
-
-    if userInput.lower() is "y":
-        while(True):
-            #enter address
-            newAddress = input("Enter a valid IPv4 address without a subnet mask or N to cancel: ")
-
-            #check escape (cancel) value
-            if newAddress.lower() is "n":
-                return(False)
-
-            #split IP address into individual octets
-            address = newAddress.split(".")
-
-            #validate number of octets
-            if len(address) == 4:
-                #validate each octet
-                while(True):
-                    for listItem in address:
-                        if listItem.isdigit():
-
-                        else:
-                            print("Invalid input. Enter the address in the format of x.x.x.x where x is an integer from 0-255.")
-                            return(False)
+        #validate number of octets in the list
+        if len(address) == 4:
+                
+            #validate each octet
+            for listItem in address:
+                    
+                #check if there are letters in the octet
+                if listItem.isdigit():
+                        
+                    #if no letters, convert to integer and continue
+                    listItem = int(listItem)
+                        
+                    #check if integer range is valid
+                    if listItem >= 0 and listItem <= 255:
                             
+                        #variable for boolean decision later
+                        status = True
+                            
+                    #if range is invalid, set status and prompt to re-enter address
+                    else:
+                        status = False
+                        print("Invalid input. Enter the address in the format of x.x.x.x where x is an integer from 0-255.")
+                        break
+                
+                #if ther are letters, prompt to re-enter address
+                else:
+                    status = False
+                    print("Invalid input. Enter the address in the format of x.x.x.x where x is an integer from 0-255.")
+                    break
+                
+            return(status)
 
-                        #if int(listItem) >= 0 and int(listItem) <= 255:
+
+def main():
+    #print dictionary content (formatted for readability)
+    print_router(router1)
+
+    #begin main loop
+    while(True):
+
+        #prompt to change mgmtIP address or quit
+        print("\nWould you like to change the mgmtIP address?")
+        userInput = str(input("Enter Y or N: "))
+
+        if userInput.lower() == "y":
+            while(True):
+                #enter address
+                newAddress = input("Enter a valid IPv4 address without a subnet mask or N to cancel: ")
+
+                #check escape (cancel) value
+                if newAddress.lower() == "n":
+                    break
+
+                #ensure address is within valid parameters, change the address in the dictionary and display the new dictionary
+                if check_address(newAddress):
+                    router1["mgmtIP"] = newAddress
+                    print_router(router1)
+                    break
+
+                else:
+                    #if not valid, display the requirements for a valid address
+                    print("Invalid input. Enter the address in the format of x.x.x.x where x is an integer from 0-255.")
 
 
-                #if address is valid, change the address in the dictionary and display the new dictionary
 
-            else:
-                #if not valid, display the requirements for a valid address
-                print("Invalid input. Enter the address in the format of x.x.x.x where x is an integer from 0-255.")
+        elif userInput.lower() == "n":
+            #break loop
+            break
 
-        #exits main loop        
-        return(False)
+        else:
+            print("Invalid input - try again.")
 
-    elif userInput.lower() is "n":
-        #end script by returning false for while loop
-        return(False)
-
-    else:
-        print("Invalid input - try again.")
+quit()
