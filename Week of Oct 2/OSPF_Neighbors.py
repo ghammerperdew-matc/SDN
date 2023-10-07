35cfeeabfe1a910959b2a283844f32ec6b3d9df5
ghammerperdew-matc <ghammerperdew@madisoncollege.edu>
2023-10-06 23:10:07 -0500

    Update OSPF_Neighbors.py

--- Week of Oct 2/OSPF_Neighbors.py
+++ Week of Oct 2/OSPF_Neighbors.py
@@ -4,9 +4,10 @@
 Author: Gavin Hammer-Perdew
 Date created: 10-3-2023
 
-Purpose: iterate through nexted dictionaries of basic device information and print it with formatting
+Purpose: iterate through a list of dictionaries of basic device information and print it with formatting
 """
 
+#List of dictionaries containing basic device information for two distribution NX-OS switches
 devices = [
     {
     "hostname": "dist-sw01",
@@ -20,8 +21,10 @@ devices = [
         }
     ]
 
+#print formatted header
 print(f"{'Host':13}", f"{'Type':10}", "Mgmt IP\n" + "-"*40)
 
+#iterate through the list of dictionaries and print the formatted information under the formatted header
 for device in devices:
 
         print(f"{device['hostname']:13}", f"{device['device_type']:10}", device["mgmt_IP"])
